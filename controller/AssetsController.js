8e6d17a82f8b544d1056256172c9093a58c98bc9
Anand Parmar <your.email@example.com>
2025-10-31 16:09:05 +0530

    path added

--- controller/AssetsController.js
+++ controller/AssetsController.js
@@ -91,7 +91,7 @@ class AssetsController {
             console.log(" Rendering the .a4 flyer container to A4 PNG.");
 
             const browser = await puppeteer.launch({
-                headless: 'new',
+                headless: true,
                 executablePath: '/root/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome',
                 args: [
                     '--no-sandbox',
